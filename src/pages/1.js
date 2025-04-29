593e801fbef72579477f7bc894ffa6a3d5865ed3
cesaradrian18 <141053368+cesaradrian18@users.noreply.github.com>
2025-04-28 23:05:41 -0600

    Agregadas las páginas 1 y 2 para prueba A/B

---
+++ src/pages/1.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const LandingOne = () => (
+  <main>
+    <h1>Landing Page 1</h1>
+    <p>Bienvenido a la versión 1 de nuestra landing.</p>
+    <a href="/2">Ir a la Landing 2</a>
+  </main>
+)
+
+export default LandingOne
---
+++ src/pages/2.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const LandingTwo = () => (
+  <main>
+    <h1>Landing Page 2</h1>
+    <p>Estás viendo la versión 2 de nuestra landing.</p>
+    <a href="/1">Volver a la Landing 1</a>
+  </main>
+)
+
+export default LandingTwo
